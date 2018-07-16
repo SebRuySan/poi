2e4858bae72084f75002df9c4f6d969b8ca07ba4
sanura-njaka <sanura@stanford.edu>
2018-07-16 11:06:51 -0700

    login page created (not implemented) w/ button--click to go to MapActivity

---
+++ app/src/main/java/me/sebastianrevel/picofinterest/LoginActivity.java
@@ -0,0 +1,29 @@
+package me.sebastianrevel.picofinterest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.btnLogin);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
