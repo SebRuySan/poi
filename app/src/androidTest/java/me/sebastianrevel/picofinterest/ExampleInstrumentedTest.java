dca94c2b36a581bb6dbd5265ad3941f0e01dae43
Ruy Gonzalez Hermosillo <ruygonzalez@fb.com>
2018-07-26 15:22:15 -0700

    Added dependencies to use Parse push notifications

--- app/src/androidTest/java/me/sebastianrevel/picofinterest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/me/sebastianrevel/picofinterest/ExampleInstrumentedTest.java
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.sebastianrevel.picofinterest", appContext.getPackageName());
+        asse
+        rtEquals("me.sebastianrevel.picofinterest", appContext.getPackageName());
     }
 }
