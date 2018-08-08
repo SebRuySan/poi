6f40c091745c914e208032efdc8654513d0c5011
Sebastian Revel <sebastianrevel@fb.com>
2018-08-08 11:17:37 -0700

    went back to unrounded

--- app/src/main/java/me/sebastianrevel/picofinterest/MainActivity.java
+++ app/src/main/java/me/sebastianrevel/picofinterest/MainActivity.java
@@ -3,7 +3,6 @@ package me.sebastianrevel.picofinterest;
 import android.Manifest;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.app.ActivityOptions;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
@@ -13,15 +12,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.PersistableBundle;
 import android.os.StrictMode;
-
 import android.os.VibrationEffect;
-import android.provider.ContactsContract;
+import android.os.Vibrator;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -29,12 +28,12 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
@@ -42,7 +41,6 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -55,9 +53,7 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
-import com.mukesh.image_processing.ImageProcessor;
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
@@ -68,7 +64,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -76,13 +71,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-import android.media.ExifInterface;
-
-import android.os.Vibrator;
-
 import me.sebastianrevel.picofinterest.Models.Pics;
-
-import static android.app.Activity.RESULT_OK;
 
 //@RuntimePermissions
 public class MainActivity extends AppCompatActivity implements FilterFragment.OnFilterInputListener, MapFragment.Callback{
@@ -344,21 +333,41 @@ public class MainActivity extends AppCompatActivity implements FilterFragment.On
         });
 
 
-        // initialize autocomplete search bar fragment and set a listener
-        placeAutoComplete = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete);
+//        // initialize autocomplete search bar fragment and set a listener
+//        placeAutoComplete = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete);
+//        placeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//            @Override
+//            public void onPlaceSelected(Place place) {
+//
+//                mapFragment.goToSearchedPlace(place);
+//                mMarker = null;
+//
+//                Log.d("Maps", "Place selected: " + place.getName());
+//            }
+//
+//            @Override
+//            public void onError(Status status) {
+//                Log.d("Maps", "An error occurred: " + status);
+//            }
+//        });
+
+
+        placeAutoComplete = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete);
+
         placeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-
+                // TODO: Get info about the selected place.
                 mapFragment.goToSearchedPlace(place);
                 mMarker = null;
-
-                Log.d("Maps", "Place selected: " + place.getName());
+                Log.i(TAG, "Place: " + place.getName());
             }
 
             @Override
             public void onError(Status status) {
-                Log.d("Maps", "An error occurred: " + status);
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
             }
         });
 
